fcad4a481c73fb66f042114913defb421639dd6f
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2022-06-17 14:00:29 +0300

    first commit

---
+++ src/main/java/com/techart/spring_patterns_test/SpringPatternsTestApplication.java
@@ -0,0 +1,13 @@
+package com.techart.spring_patterns_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPatternsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPatternsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techart/spring_patterns_test/SpringPatternsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techart.spring_patterns_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPatternsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
